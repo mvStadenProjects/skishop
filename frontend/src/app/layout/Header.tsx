37d986ff08c596f7867d8b1895441f4feed411c6
Marius Van Staden <mvstadenProjects@gmail.com>
2024-07-05 09:11:09 +0200

    Added React Router and Product Details Page

--- frontend/src/app/layout/Header.tsx
+++ frontend/src/app/layout/Header.tsx
@@ -1,15 +1,101 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  {
+    title: "catalog",
+    path: "/catalog",
+  },
+  {
+    title: "about",
+    path: "/about",
+  },
+  {
+    title: "contact",
+    path: "/contact",
+  },
+];
+const rightLinks = [
+  {
+    title: "login",
+    path: "/login",
+  },
+  {
+    title: "register",
+    path: "/register",
+  },
+];
 
 interface Props {
   darkMode: boolean;
   handleThemeChange: () => void;
 }
+
+const navStyles = {
+  textDecoration: "none",
+  color: "inherit",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
+
 const Header = ({ darkMode, handleThemeChange }: Props) => {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography variant="h6">SKI-SHOP</Typography>
-        <Switch onChange={handleThemeChange} checked={darkMode} />
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}>
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6" component={NavLink} to="/" sx={navStyles}>
+            SKI-SHOP
+          </Typography>
+          <Switch onChange={handleThemeChange} checked={darkMode} />
+        </Box>
+
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={title} sx={navStyles}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem
+                component={NavLink}
+                to={path}
+                key={title}
+                sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
---
+++ frontend/src/features/contact/ContactPage.tsx
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+
+const ContactPage = () => {
+  return <Typography variant="h2">Contact Page</Typography>;
+};
+export default ContactPage;
